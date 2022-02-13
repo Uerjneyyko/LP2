f69d3c5bea4ef1eddc06e2c98db7cc2a431bfc28
Uerjneyyko <97130898+Uerjneyyko@users.noreply.github.com>
2022-02-13 13:50:26 -0300

    Add files via upload

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -20,15 +20,34 @@ class Hello2DFrame extends JFrame {
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 350);
         this.setVisible(true);
+        getContentPane().setBackground(Color.red);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
+        g2d.setPaint(Color.black);
         int w = getWidth();
         int h = getHeight();
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
+        int p=0;
+        for(p=0 ; p <= getHeight() ; p += 10){
+            g.drawLine(0, p, p, getHeight());
+         }
+
+        for(p=getHeight() ; p >=0 ; p -= 10){
+            g.drawLine(0, p, getHeight() - p, 0);
+     }
+     
+        for(p=0 ; p <= getHeight() ; p +=10){
+            g.drawLine(getWidth(), p, getWidth() - p, getHeight());
+     }
+     
+        for(p=getHeight() ; p >=0 ; p -= 10){
+            g.drawLine(getWidth(), p, getWidth() - (getHeight() - p), 0);
+     }
     }
+    
+
 }
