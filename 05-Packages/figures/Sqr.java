e4dbc609b889f6c519b1a6f95e96330266d63e43
Uerjneyyko <97130898+Uerjneyyko@users.noreply.github.com>
2022-03-06 21:31:19 -0300

    Update and rename Sqr to Sqr.java

--- 05-Packages/figures/Sqr.java
+++ 05-Packages/figures/Sqr.java
@@ -1,10 +1,12 @@
 package figures;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Sqr {
     int x, y;
     int s;
+    float r,g,b;
 
     public Sqr (int x, int y, int s) {
         this.x = x;
@@ -19,6 +21,18 @@ public class Sqr {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        randColor();
+        g2d.setColor(new Color(this.r,this.g,this.b));
+        g2d.fillRect(this.x, this.y, this.s,this.s);
+        randColor();
+        g2d.setColor(new Color(this.r,this.g,this.b));
         g2d.drawRect(this.x,this.y, this.s,this.s);
+    }
+
+    void randColor(){
+        Random rand = new Random();
+        this.r=rand.nextFloat();
+        this.g=rand.nextFloat();
+        this.b=rand.nextFloat();
     }
 }
