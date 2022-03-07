f1930f0da6ccea6ebc872e596958d225c810aa49
Uerjneyyko <97130898+Uerjneyyko@users.noreply.github.com>
2022-03-06 21:29:37 -0300

    Update Rect.java

--- 05-Packages/figures/Rect.java
+++ 05-Packages/figures/Rect.java
@@ -1,10 +1,12 @@
 package figures;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Rect {
     int x, y;
     int w, h;
+    float r,g,b;
 
     public Rect (int x, int y, int w, int h) {
         this.x = x;
@@ -20,6 +22,18 @@ public class Rect {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        randColor();
+        g2d.setColor(new Color(this.r,this.g,this.b));
+        g2d.fillRect(this.x, this.y, this.w , this.h);
+        randColor();
+        g2d.setColor(new Color(this.r,this.g,this.b));
         g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+
+    void randColor(){
+        Random rand = new Random();
+        this.r=rand.nextFloat();
+        this.g=rand.nextFloat();
+        this.b=rand.nextFloat();
     }
 }
